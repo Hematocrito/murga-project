3789c715a0d26131e91109b54bd569f7221b8c76
hema <aussonia@gmail.com>
2024-11-28 13:45:49 -0300

    Incorporación de la conexión a bd

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState } from 'react';
+import { gql, useMutation } from "@apollo/client";
 
 interface AuthContextType {
   isAuthenticated: boolean;
@@ -6,18 +7,55 @@ interface AuthContextType {
   logout: () => void;
 }
 
+const AUTENTICAR_USUARIO = gql`
+    mutation AutenticarUsuario($input: AutenticarInput) {
+        autenticarUsuario(input: $input) {
+            token
+        }
+    }
+`;
+
 const AuthContext = createContext<AuthContextType | null>(null);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  //Mutation para crear nuevos usuarios en apollo
+  const [ autenticarUsuario ] = useMutation(AUTENTICAR_USUARIO);
+
   const login = async (username: string, password: string) => {
     // Simulated authentication - In production, replace with real API call
+
+    try {
+      const { data } = await autenticarUsuario({
+        variables: {
+            input:{
+                email: username,
+                password                            
+            }
+        }
+      });
+      console.log(data);
+
+      //Guardar el token en local storage
+      const { token } = data.autenticarUsuario;
+      localStorage.setItem('token', token);
+
+      setIsAuthenticated(true);
+      return true;
+      
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+
+
+    /*
     if (username === 'admin' && password === 'admin') {
       setIsAuthenticated(true);
       return true;
     }
-    return false;
+    */
   };
 
   const logout = () => {
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,30 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { HttpLink } from '@apollo/client';
+
+
+//Leer el storage almacenado
+const token = localStorage.getItem('token');
+const httpLink = {
+  //uri: 'http://localhost:4000/',
+  uri: 'https://vast-hollows-51774-ff90a6c856e2.herokuapp.com/',
+  headers: {
+    authorization: `Bearer ${token}`
+  }
+}
+
+const client = new ApolloClient({
+  link: new HttpLink(httpLink),
+  cache: new InMemoryCache()
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>
 );
